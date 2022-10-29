0b1327d7ab32a9b39185ab14fcae870df1ac77e4
hsjoihs <jekto.vatimeliju@gmail.com>
2022-10-29 23:47:01 +0900

    Split off apperror.rs and tokenize.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![warn(clippy::pedantic)]
-use std::fmt;
 use std::io::Write;
+
+use apperror::AppError;
+use tokenize::{Token, TokenPayload};
 
 fn main() -> std::io::Result<()> {
     let input = std::env::args().nth(1).expect("入力が与えられていません");
 
-    let tokens = tokenize(&input).unwrap();
+    let tokens = tokenize::tokenize(&input).unwrap();
 
     let file = std::fs::File::create("a.out")?;
     let mut writer = std::io::BufWriter::new(file);
@@ -87,124 +89,6 @@ fn parse_and_codegen(
         }),
     }
 }
+mod apperror;
 
-#[derive(Debug, Clone)]
-struct AppError {
-    message: String,
-    input: String,
-    pos: usize,
-}
-
-impl fmt::Display for AppError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f, "{}", self.input)?;
-        write!(f, "{}^ {}", " ".repeat(self.pos), self.message)?;
-        Ok(())
-    }
-}
-
-impl std::error::Error for AppError {}
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-enum TokenPayload {
-    Num(u8),
-    Add,
-    Sub,
-    Eof,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct Token {
-    payload: TokenPayload,
-    pos: usize,
-}
-
-#[test]
-fn tokenize_test() {
-    assert_eq!(
-        tokenize("5 - 3").unwrap(),
-        vec![
-            Token {
-                payload: TokenPayload::Num(5),
-                pos: 0
-            },
-            Token {
-                payload: TokenPayload::Sub,
-                pos: 2
-            },
-            Token {
-                payload: TokenPayload::Num(3),
-                pos: 4
-            },
-            Token {
-                payload: TokenPayload::Eof,
-                pos: 5
-            }
-        ]
-    );
-}
-
-fn tokenize(input: &str) -> Result<Vec<Token>, AppError> {
-    let mut ans = vec![];
-    let mut iter = input.chars().enumerate().peekable();
-    while let Some(&(pos, c)) = iter.peek() {
-        match c {
-            ' ' => {
-                iter.next();
-                continue;
-            }
-            '+' => {
-                iter.next();
-                ans.push(Token {
-                    payload: TokenPayload::Add,
-                    pos,
-                });
-            }
-            '-' => {
-                iter.next();
-                ans.push(Token {
-                    payload: TokenPayload::Sub,
-                    pos,
-                });
-            }
-            '0'..='9' => ans.push(Token {
-                payload: TokenPayload::Num(parse_num(&mut iter).map_err(|message| AppError {
-                    message,
-                    input: input.to_string(),
-                    pos,
-                })?),
-                pos,
-            }),
-            _ => {
-                return Err(AppError {
-                    message: "トークナイズできない不正な文字です".to_string(),
-                    input: input.to_string(),
-                    pos,
-                })
-            }
-        }
-    }
-    ans.push(Token {
-        payload: TokenPayload::Eof,
-        pos: input.len(),
-    });
-    Ok(ans)
-}
-
-fn parse_num(
-    iter: &mut std::iter::Peekable<impl Iterator<Item = (usize, char)>>,
-) -> Result<u8, String> {
-    let mut s = String::new();
-
-    while let Some(&(_, c)) = iter.peek() {
-        if c.is_ascii_digit() {
-            s.push(c);
-            iter.next();
-        } else {
-            break;
-        }
-    }
-
-    s.parse::<u8>()
-        .map_err(|_| "入力が符号なし8bit整数ではありません".to_string())
-}
+mod tokenize;
